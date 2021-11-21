3525f894d013b0dee56e5819907a4334190cdc8a
sahabiswarup <48620172+sahabiswarup@users.noreply.github.com>
2021-11-21 10:48:19 +0530

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace e_Travel
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            WebMessenger.Show(this, "Gym Pass Type Could Not Load" + "Error Information", "Information", DialogTypes.Information);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="e_Travel.Global" Language="C#" %>
